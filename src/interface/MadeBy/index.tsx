bcc5f3ee81f94da820218560af590d97e9101f05
Yin Shanyang <me@yinshanyang.com>
2024-04-03 16:37:04 +0800

    chore: update Swarm url

--- src/interface/MadeBy/index.tsx
+++ src/interface/MadeBy/index.tsx
@@ -1,8 +1,11 @@
 import * as React from 'react'
 
 const MadeBy = () => (
-  <div className='dn db-ns absolute bottom-1 right-1 f7 lh-solid text-reversed-40'>
-    Made by <a className='link underline text-reversed-40' href='https://swarm.is/'>Swarm</a>
+  <div className="dn db-ns absolute bottom-1 right-1 f7 lh-solid text-reversed-40">
+    Made by{' '}
+    <a className="link underline text-reversed-40" href="https://swarml.ing/">
+      Swarm
+    </a>
   </div>
 )
 
